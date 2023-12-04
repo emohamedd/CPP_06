e5be93078f81f165045db9c5ef56253b3fee7da6
emohamedd <a.elgaouej@gmail.com>
2023-12-04 16:17:09 +0100

    to char and to int

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: emohamed <emohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 17:09:16 by emohamed          #+#    #+#             */
-/*   Updated: 2023/12/04 15:52:23 by emohamed         ###   ########.fr       */
+/*   Updated: 2023/12/04 16:15:59 by emohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,20 @@ ScalarConverter& ScalarConverter::operator=(ScalarConverter const &other)
 
 char str_to_char(std::string str)
 {
-    if (str.length() == 1)
-    {
+    if (str.length() == 1 && !std::isdigit(str[0]))
         return static_cast<char>(str[0]);
-    }
-    else
-        return 0;
+    int char_to_int = std::atoi(str.c_str());
+    char c = static_cast<char>(char_to_int);
+    return c;
 }
 
 int str_to_int(std::string str)
 {
-    if (str.length() == 1)
-    {
+    const char *s = str.c_str();
+    if (str.length() == 1 && !std::isdigit(str[0]))
         return static_cast<int>(str[0]);
-        
-    }
-    else
-        return 0;
+    int char_to_int = std::atoi(s);
+    return char_to_int;
 }
 
 float str_to_float(std::string str)
@@ -62,6 +59,7 @@ void ScalarConverter::convert(std::string av)
         int i = str_to_int(av);
         float f = str_to_float(av);
         double d = str_to_double(av);
+        // std::cout << c << std::endl << i << std::endl << f << std::endl << d << std::endl;
         if (c == 0 && i == 0 && f == 0 && d == 0)
             throw std::invalid_argument("Invalid argument");
         else
@@ -72,10 +70,7 @@ void ScalarConverter::convert(std::string av)
             else
                 std::cout << "impossible" << std::endl;
             std::cout << "int: ";
-            if (i >= 32 && i <= 126)
                 std::cout  << i  << std::endl;
-            else
-                std::cout << "impossible" << std::endl;
             std::cout << "float: "  << f << "f" << std::endl;
             std::cout << "double: "  << d << std::endl;
         }
